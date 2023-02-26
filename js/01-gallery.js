72195e98260bdea02f8387b86a54f446da7344cb
Viacheslav-Penskoy <114249044+Viacheslav-Penskoy@users.noreply.github.com>
2023-02-26 12:10:51 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -26,3 +26,25 @@ galleryItems.forEach(element => {
     items.push(galleryItem)
 });
 
+gallery.append(...items);
+
+gallery.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return
+    }
+
+    const selectedImage = e.target.getAttribute('data-source')
+
+    const instance = basicLightbox.create(`
+    <img src="${selectedImage}" width="800" height="600">
+`)
+
+    instance.show()
+    
+    gallery.addEventListener('keydown', e => {
+        if (e.key === 'Escape') {
+            instance.close()
+        }
+    })
+});
