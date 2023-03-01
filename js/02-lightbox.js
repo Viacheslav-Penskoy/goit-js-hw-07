de50463570a2b25b2f277dfb5029f73e1389e61b
Viacheslav-Penskoy <114249044+Viacheslav-Penskoy@users.noreply.github.com>
2023-03-01 11:23:17 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,25 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const gallery = document.querySelector('.gallery')
-const items = []
+const gallery = document.querySelector('.gallery');
+const items = [];
 
 galleryItems.forEach(element => {
-	const galleryLink = document.createElement('a')
-	galleryLink.className = 'gallery__link'
-	galleryLink.href = element.original
-	const galleryImage = document.createElement('img')
-	galleryImage.className = 'gallery__image'
-	galleryImage.src = element.preview
-	galleryImage.setAttribute('title', element.description)
-	galleryImage.alt = element.description
+    const galleryLink = document.createElement('a')
+    galleryLink.className = 'gallery__link'
+    galleryLink.href = element.original
+    
+    const galleryImage = document.createElement('img')
+    galleryImage.className = 'gallery__image'
+    galleryImage.src = element.preview
+    galleryImage.setAttribute('title', element.description)
+    galleryImage.alt = element.description
 
-	galleryLink.append(galleryImage)
-	items.push(galleryLink)
-})
-gallery.append(...items)
+    galleryLink.append(galleryImage)
+    items.push(galleryLink)
+});
 
-new SimpleLightbox('.gallery a', {
-	captionDelay: 250
-})
+gallery.append(...items);
 
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250
+});
