aa974628f23609c0aed7de43f7f26474f06e69c9
Viacheslav-Penskoy <114249044+Viacheslav-Penskoy@users.noreply.github.com>
2023-03-01 11:29:27 +0200

    Update 02-lightbox.js
    1.2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,6 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const gallery = document.querySelector('.gallery');
+
 const items = [];
 
 galleryItems.forEach(element => {
